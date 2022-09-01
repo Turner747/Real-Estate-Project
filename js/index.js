46afa36352a5c91005e5492647f08dfef85693a8
Joshua Turner <joshturner747@hotmail.com>
2022-09-01 23:48:51 +1000

    final commit, project complete

--- js/index.js
+++ js/index.js
@@ -14,3 +14,17 @@ function resetSearch(){
     document.getElementById("renterSearch").style.display = "none";
     document.getElementById("buyerSearch").style.display = "none";
 }
+
+function sendContact(){
+    const alertPlaceholder = document.getElementById('alertPlaceholder');
+
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-success alert-dismissible" role="alert">`,
+        `   <div><p>Message sent successfully, we will be in touch</p></div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('');
+
+    alertPlaceholder.append(wrapper);
+}
